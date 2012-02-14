88c706f609ba2e796a1ca74876a3efa8b4a98e6e
Gareth Kendrick <garethk@gmail.com>
2012-02-14 11:38:15 -0700

    Finish user edit, update, index and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+  before_filter :authenticate, :only => [:index, :edit, :update, :destroy]
+  before_filter :correct_user, :only => [:edit, :update]
+  before_filter :admin_user,   :only => :destroy
+  before_filter :not_admin,    :only => :destroy
+
+  def index
+    @title = "All users"
+    @users = User.paginate(:page => params[:page])
+  end
 
   def show
     @user = User.find(params[:id])
@@ -6,11 +15,17 @@ class UsersController < ApplicationController
   end
 
   def new
+    if signed_in? 
+      redirect_to(root_path)
+    end
     @user = User.new
     @title = "Sign up"
   end
 
   def create
+    if signed_in? 
+      redirect_to(root_path)
+    end
     @user = User.new(params[:user])
     if @user.save
       sign_in @user
@@ -23,4 +38,50 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+
+  def edit
+    @user = User.find(params[:id])
+    @title = "Edit user"
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated."
+      redirect_to @user
+    else
+      @title = "Edit user"
+      render 'edit'
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_path
+  end
+
+  private
+    
+    def authenticate
+      deny_access unless signed_in?
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
+    end
+
+    def not_admin
+      @user = User.find(params[:id])
+      if @user.admin?
+        flash[:error] = "User is an admin and cannot be destroyed."
+        redirect_to(users_path) 
+      end
+    end
+
 end
